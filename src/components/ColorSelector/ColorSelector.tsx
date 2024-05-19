a78e6ca8eda66a502b2ede66d12aac1ea8809d99
freirart <artur.santos@stone.com.br>
2024-05-19 17:18:39 -0300

    adds color selector component

---
+++ src/components/ColorSelector/ColorSelector.tsx
@@ -0,0 +1,46 @@
+import { FC, useRef } from 'react';
+
+import styles from './styles.module.less';
+
+interface ColorSelectorProps {
+  setNewColor: (v: string) => void;
+  defaultColor?: string;
+}
+
+const ColorSelector: FC<ColorSelectorProps> = ({
+  setNewColor,
+  defaultColor
+}) => {
+  const inputValue = useRef<HTMLInputElement>(null);
+
+  const shouldShowGoDefaultBtn =
+    defaultColor &&
+    inputValue.current &&
+    inputValue.current.value !== defaultColor;
+
+  const goDefault = () => {
+    if (shouldShowGoDefaultBtn) {
+      setNewColor(defaultColor);
+      inputValue.current.value = defaultColor;
+    }
+  };
+
+  return (
+    <div className="flex">
+      {shouldShowGoDefaultBtn ? (
+        <button className="text-sm underline" onClick={goDefault}>
+          Retornar para o padrão
+        </button>
+      ) : null}
+      <input
+        ref={inputValue}
+        className={styles.colorInput}
+        type="color"
+        onChange={(e) => setNewColor(e.target.value)}
+        defaultValue={defaultColor}
+      />
+    </div>
+  );
+};
+
+export default ColorSelector;
