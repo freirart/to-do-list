73adfd6040c9fbde43affb64b9134330f096c104
freirart <artur.santos@stone.com.br>
2024-05-19 11:03:12 -0300

    initial project setup

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ToDo from "./ToDo";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ToDo />
+  </React.StrictMode>
+);
