30ba03880310cffed49e6ea6527ac2eecc370112
freirart <artur.santos@stone.com.br>
2024-05-19 18:30:32 -0300

    adds fonts

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,12 @@ import ToDoApp from './ToDoApp';
 
 import './globals.css';
 
+import '@fontsource/lato/100.css';
+import '@fontsource/lato/300.css';
+import '@fontsource/lato/400.css';
+import '@fontsource/lato/700.css';
+import '@fontsource/lato/900.css';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ToDoApp />
