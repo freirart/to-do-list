b8290ceab8a874836d27bbb817b4a9bf50c6a503
freirart <artur.santos@stone.com.br>
2024-05-19 16:11:14 -0300

    adds tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import ToDoApp from './ToDoApp';
 import Store from './store';
 
+import './globals.css';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Store.Provider initialState={{ todos: [] }}>
