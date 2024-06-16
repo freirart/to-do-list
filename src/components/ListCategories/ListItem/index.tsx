891defe0fd2f42945a8b67915e1d7fa26f6272fe
freirart <artur.santos@stone.com.br>
2024-06-16 13:57:07 -0300

    conditional color on category list

--- src/components/ListCategories/ListItem/index.tsx
+++ src/components/ListCategories/ListItem/index.tsx
@@ -8,6 +8,7 @@ import AddCategoryInput from './AddCategoryInput';
 
 interface ListItemInterface {
   item: CategoryInterface;
+  isChild?: boolean;
 }
 
 export const getDefaultClasses = () => `
@@ -16,7 +17,10 @@ export const getDefaultClasses = () => `
   transition-colors
 `;
 
-export default function ListItem({ item }: ListItemInterface) {
+export default function ListItem({
+  item,
+  isChild
+}: ListItemInterface) {
   const filterName = useFilterName();
   const updateFilterName = useUpdateFilterName();
 
@@ -30,14 +34,16 @@ export default function ListItem({ item }: ListItemInterface) {
     updateFilterName(itemParam.name);
   };
 
-  const className = `
-    ${getDefaultClasses()}
-    ${filterName === item.name ? 'text-primary' : ''}
-  `;
-
   if (!item.children) {
     return (
-      <li className={className} onClick={getOnClickFn(item)}>
+      <li
+        data-is-child={isChild}
+        data-selected={!isChild && filterName === item.name}
+        className="cursor-pointer hover:text-primary/50 transition-colors
+          data-[selected=true]:text-primary data-[is-child=true]:underline
+          data-[is-child=true]:decoration-primary"
+        onClick={getOnClickFn(item)}
+      >
         {item.name}
       </li>
     );
@@ -46,11 +52,19 @@ export default function ListItem({ item }: ListItemInterface) {
   return (
     <li>
       <details>
-        <summary className={className}>Categories</summary>
+        <summary
+          data-children-selected={
+            !!item.children?.find((i) => i.name === filterName)
+          }
+          className="cursor-pointer hover:text-primary/50 transition-colors
+            data-[children-selected=true]:text-primary"
+        >
+          Categories
+        </summary>
         <ul className="text-lg leading-10">
           <AddCategoryInput />
           {item.children.map((childrenItem, index) => (
-            <ListItem key={index} item={childrenItem} />
+            <ListItem isChild key={index} item={childrenItem} />
           ))}
         </ul>
       </details>
